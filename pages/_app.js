fe7db11d89a2973bef9e4ec7b0de137b0cc18c43
matheusgondra <matheusgondra951@gmail.com>
2022-04-13 09:38:15 -0300

    Inicio do alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,42 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    paddign: 0;
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color: #D9E6F6;
+    font-family: sans-serif;
   }
+
+  #_next {
+    display: flex
+    min-height: 100vh;
+    flex-direction: column
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+  ul {
+    padding: 0;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: 'red',
   },
 }
 
